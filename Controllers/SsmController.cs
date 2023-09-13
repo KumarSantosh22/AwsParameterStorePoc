69861345b2df48e2db4f0dfcaa5d70a07d6e5c36
KumarSantosh22 <santoshcse97@gmail.com>
2023-09-13 11:26:15 +0530

    initial commit

---
+++ Controllers/SsmController.cs
@@ -0,0 +1,52 @@
+﻿using Amazon.SimpleSystemsManagement.Model;
+using Amazon.SimpleSystemsManagement;
+using Microsoft.AspNetCore.Mvc;
+using System.Text;
+
+namespace AwsParameterStorePoc.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SsmController : ControllerBase
+    {
+        private readonly AmazonSimpleSystemsManagementClient _ssmClient;
+        public SsmController()
+        {
+            _ssmClient = new AmazonSimpleSystemsManagementClient(Amazon.RegionEndpoint.USEast1);
+        }
+
+        [HttpGet("[action]/{key}")]
+        public async Task<IActionResult> GetParams([FromRoute] string key)
+        {
+            StringBuilder stringBuilder= new StringBuilder();
+            stringBuilder.AppendLine($"Params:{key} : Initiated");
+            try
+            {
+                string res = await GetParameter(key);
+                stringBuilder.AppendLine(res);
+            }
+            catch (Exception ex)
+            {
+               stringBuilder.AppendLine(ex.ToString());
+                if(ex.InnerException is not null)
+                {
+                    stringBuilder.AppendLine(ex.InnerException.ToString());
+                }
+            }
+            stringBuilder.AppendLine($"Params:{key} : Completed");
+            return Ok(stringBuilder.ToString());
+        }
+
+        public async Task<string> GetParameter(string key)
+        {
+            GetParameterRequest getRequest = new()
+            {
+                Name = key,
+                WithDecryption = true // Set to true if the parameter value is encrypted
+            };
+            GetParameterResponse getResponse = await _ssmClient.GetParameterAsync(getRequest);
+            return getResponse.Parameter.Value;
+          
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,45 @@
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace AwsParameterStorePoc;
+
+public class Startup
+{
+    public Startup(IConfiguration configuration)
+    {
+        Configuration = configuration;
+    }
+
+    public IConfiguration Configuration { get; }
+
+    // This method gets called by the runtime. Use this method to add services to the container
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddControllers();
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseRouting();
+
+        app.UseAuthorization();
+
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllers();
+            endpoints.MapGet("/", async context =>
+            {
+                var obj = new { msg= "Welcome to running ASP.NET Core on AWS Lambda" };
+                await context.Response.WriteAsync(JsonSerializer.Serialize(obj));
+            });
+        });
+    }
+}
